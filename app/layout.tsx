69ad74a8f9f0dc17b67bbdb2dee693c12e6576b6
arobles02 <arobles02@hotmail.com>
2026-03-24 14:30:54 -0600

    Update layout.tsx/casmbio en SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Serenity",
-  description: "Serenity Surgery Planner, bariatric surgery in Puebla Mexico, Rhinoplasty in Puebla Mexico , Mammoplasty  in Puebla Mexico , Gastric Sleeve Puebla Mexico.", 
+  description: "Serenity Surgery Planner, bariatric surgery in Puebla Mexico, Rhinoplasty in Puebla Mexico, Mammoplasty in Puebla Mexico, Gastric Sleeve Puebla Mexico", 
   icons: "/Logo.png"
 };
 
